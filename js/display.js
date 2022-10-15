8f645e97e6e6a8aaa01d2f0508dd054cdbe02ee0
Wagyx <ecirblo@hotmail.com>
2022-10-15 18:09:30 +0200

    makes the lexique page

--- js/display.js
+++ js/display.js
@@ -1,22 +1,48 @@
-function displayCategory(category){
-
+function displayCategory(category) {
     const content = document.getElementById("galleryContent");
     for (let arg of category.words) {
         // make a list item
-        const element = document.createElement( 'div' );
+        const element = document.createElement('div');
         element.className = 'list-item';
-
-        const nameElement = document.createElement( 'p' );
+        content.appendChild(element);
+        const nameElement = document.createElement('p');
         nameElement.innerText = VOCABULARY[arg].name;
         element.appendChild(nameElement);
-        const videoElement = document.createElement( 'video' );
+        const videoElement = document.createElement('video');
         videoElement.src = VOCABULARY[arg].url;
         videoElement.controls = true;
-        // console.log(videoElement);
         element.appendChild(videoElement);
-        content.appendChild( element );
-
     }
-
 }
 
+function displayList() {
+    const content = document.getElementById("listContent");
+    words = Object.keys(VOCABULARY);
+    words.sort((a, b) => a.normalize('NFD') > b.normalize('NFD'));
+    for (let arg of words) {
+        // make a list item
+        const element = document.createElement('div');
+        element.className = 'list-item';
+        element.onclick = displayVideo;
+        content.appendChild(element);
+
+        const nameElement = document.createElement('p');
+        nameElement.innerText = VOCABULARY[arg].name;
+        nameElement.onclick = displayVideo;
+        element.appendChild(nameElement);
+    }
+}
+
+function displayVideo(event) {
+    let element = event.target;
+    if (element.tagName.toLowerCase() == 'p') {
+        element = element.parentElement;
+    }
+    if ((element.className == 'list-item') && (element.children.length == 1)) {
+        const name = element.children[0].innerText;
+        videoElement = document.createElement("video");
+        videoElement.src = VOCABULARY[name].url;
+        videoElement.controls = true;
+        element.appendChild(videoElement);
+    }
+}
